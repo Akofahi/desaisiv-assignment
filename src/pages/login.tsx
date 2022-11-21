d1bf62add925ff80191d08748d00cabb124f46cb
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-22 02:00:18 +0300

    authentication working

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,18 +7,48 @@ import {
 } from "react-firebase-hooks/auth";
 import firebase from "../firebaseconfig.js";
 import { getAuth } from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(firebase);
 function Login() {
-  const [signInWithEmailAndPassword, user, loading, error] =
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, userdata, loading, error] =
     useSignInWithEmailAndPassword(auth);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email,password)
+  };
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (userdata) {
+    return (
+      <div>
+        <p>Signed In User: {userdata.user.email}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="Login">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -26,12 +56,17 @@ function Login() {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" >
           Submit
         </Button>
         <p>
