c4f3f202aec701f5aea5d78bf60812de60121313
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-22 00:13:50 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Register from './pages/register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Register/>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        
+        <div>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+function Register() {
+  return <h1>Register</h1>;
 }
 
 export default App;
