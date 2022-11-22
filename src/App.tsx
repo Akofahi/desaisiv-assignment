d983e319dbd175d647107a8f11c8e688481cd325
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-22 23:18:23 +0300

    use yarn

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import {
 } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import { Container } from 'react-bootstrap';
-
+// import { } from 'react-firebase-hooks';
 
 const routes = [
   {
