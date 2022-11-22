99f35975d3b99aa8f02a78823597e65e55889e77
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-23 00:44:10 +0300

    ----

--- src/pages/emptable.tsx
+++ src/pages/emptable.tsx
@@ -1,35 +1,64 @@
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
+import { getFirestore, collection } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { firebase } from "../firebaseconfig";
+import React from "react";
 
-const employees = [
-  {
-    firstName: 'Jacop',
-    lastName: 'Smith',
-  },
-  {
-    firstName: 'Mark',
-    lastName: 'Scot',
-  }
-]
 function Emptable() {
+  const [value, loading, error] = useCollection(
+    collection(getFirestore(firebase), "Users"),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
   return (
     <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          employees.map((emp) => <tr>
-            <td>{emp.firstName}</td>
-            <td>{emp.lastName}</td>
-          </tr>)
-        }
+     <thead>
+       <tr>
+         <th>First Name</th>
+         <th>Last Name</th>
+       </tr>
+     </thead>
+     <tbody>
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Collection: Loading...</span>}
+        {value && (
+          <span>
+            {value.docs.map((doc) => (
+              <React.Fragment key={doc.id}>
+                <tr>
+                  <td>{JSON.stringify(doc.data().firstName)},{" "}</td>
+                  <td>{JSON.stringify(doc.data().lastName)},{" "}</td>
+                </tr>
+                
+              </React.Fragment>
+            ))}
+          </span>
+        )}
+     </tbody>
+   </Table>
+ );
 
-      </tbody>
-    </Table>
-  );
 }
+//   <Table striped bordered hover size="sm">
+//     <thead>
+//       <tr>
+//         <th>First Name</th>
+//         <th>Last Name</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {
+//         employees.forEach(element => {
+
+//         });((emp) => <tr>
+//           <td>{emp.firstName}</td>
+//           <td>{emp.lastName}</td>
+//         </tr>)
+//       }
+
+//     </tbody>
+//   </Table>
+// );
 
 export default Emptable;
