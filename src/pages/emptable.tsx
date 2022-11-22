6f8bd8f4a1b3808eb73699aa4dc9b495c4d57128
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-23 01:07:09 +0300

    Update emptable.tsx

--- src/pages/emptable.tsx
+++ src/pages/emptable.tsx
@@ -12,32 +12,31 @@ function Emptable() {
     }
   );
   return (
+
     <Table striped bordered hover size="sm">
-     <thead>
-       <tr>
-         <th>First Name</th>
-         <th>Last Name</th>
-       </tr>
-     </thead>
-     <tbody>
-        {error && <strong>Error: {JSON.stringify(error)}</strong>}
-        {loading && <span>Collection: Loading...</span>}
-        {value && (
-          <span>
-            {value.docs.map((doc) => (
-              <React.Fragment key={doc.id}>
-                <tr>
-                  <td>{JSON.stringify(doc.data().firstName)},{" "}</td>
-                  <td>{JSON.stringify(doc.data().lastName)},{" "}</td>
-                </tr>
-                
-              </React.Fragment>
-            ))}
-          </span>
-        )}
-     </tbody>
-   </Table>
- );
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+
+
+        {value.docs.map((doc) => (
+          <React.Fragment key={doc.id}>
+            <tr>
+              <td>{JSON.stringify(doc.data().firstName)},{" "}</td>
+              <td>{JSON.stringify(doc.data().lastName)},{" "}</td>
+            </tr>
+          </React.Fragment>
+        ))}
+
+      </tbody>
+    </Table>
+  );
 
 }
 //   <Table striped bordered hover size="sm">
