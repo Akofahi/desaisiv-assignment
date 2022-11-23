5e0c7173be69e43c8257b78e446a4bdda0dd0471
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-23 13:26:54 +0300

    Update register.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -56,17 +56,17 @@ function Register() {
       }
       <Form onSubmit={handleSubmit}>
         <Form.Group className="sm-1" controlId="formBasicName">
-          <Form.Label>First Name</Form.Label>
+          <Form.Label className="mt-4" >First Name</Form.Label>
           <Form.Control type="text" placeholder="Enter First Name" value={fName}
             onChange={(e) => setfName(e.target.value)} />
-          <Form.Label>Last Name</Form.Label>
+          <Form.Label className="mt-4" >Last Name</Form.Label>
           <Form.Control type="text" placeholder="Enter Last Name" value={lName}
             onChange={(e) => setlName(e.target.value)} />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
         
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label className="mt-4" >Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" value={email}
             onChange={(e) => setEmail(e.target.value)} />
           <Form.Text className="text-muted">
@@ -76,14 +76,14 @@ function Register() {
 
         <CountryDropdown
           id="UNIQUE_ID"
-          className="YOUR_CSS_CLASS"
+          className="mt-4"
           preferredCountries={["gb", "us"]}
           value={country}
           onChange={(e) => setCountry(e.target.value)}
         ></CountryDropdown>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
+          <Form.Label className="mt-4" >Password</Form.Label>
           <Form.Control type="password" placeholder="Password" value={password}
             onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
