720384e6b52e3036afc6a150c0e86336fc23a984
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-11-23 13:25:34 +0300

    Update register.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -64,6 +64,7 @@ function Register() {
             onChange={(e) => setlName(e.target.value)} />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
+        
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" value={email}
@@ -80,14 +81,13 @@ function Register() {
           value={country}
           onChange={(e) => setCountry(e.target.value)}
         ></CountryDropdown>
+
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" value={password}
             onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
